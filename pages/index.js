7afdfd99cec37dcd4ead0ed1c1746093017efe46
jeongung.yang <jeongung.yang@readnumber.com>
2022-11-07 10:14:45 +0900

    에러 수정

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Let's get started by editing{' '}
+          Lets Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
